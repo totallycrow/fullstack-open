1bf5f30724fb54e67d48c73d32762ee7bb151bdb
Adrian Gajda <adrian.gajda@hotmail.com>
2022-06-30 00:32:38 +0100

    code along

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => console.log(response.data))
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)

3ea706893d54f85efce834e702b90ffb0dade640
Adrian Gajda <adrian.gajda@hotmail.com>
2022-06-30 17:09:07 +0100

    code along

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import { useState, useEffect } from 'react'
 import Note from "./components/Note";
 import axios from 'axios'
 import noteService from './services/notes'
+import Notification from './components/Notification';
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+    </div>
+  )
+}
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("a new note");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -45,7 +61,12 @@ const toggleImportanceOf = id => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(`the note ${note.content} was already deleted from server`)
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -62,6 +83,7 @@ const toggleImportanceOf = id => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
 
       <div>
         {" "}
@@ -81,6 +103,7 @@ const toggleImportanceOf = id => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">Save Test from Remote</button>
       </form>
+      <Footer />
     </div>
   );
 };

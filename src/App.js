7363a4fb82f723d7786f73c7ec24fe9e7df01739
Adrian Gajda <adrian.gajda@hotmail.com>
2022-07-05 10:23:37 +0100

    work

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import Note from "./components/Note";
-import axios from 'axios'
+import axios from "axios";
 
 const App = (props) => {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  
-  console.log('render', notes.length, 'notes')
+    console.log("effect");
+    axios.get("http://localhost:3001/api/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
 
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       date: new Date(),
       important: Math.random() < 0.5,
-    }
-  
+    };
+
     axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        console.log(response)
-        setNotes(notes.concat(response.data))
-        setNewNote('')
-      })
-  }
+      .post("http://localhost:3001/api/notes", noteObject)
+      .then((response) => {
+        console.log(response);
+        setNotes(notes.concat(response.data));
+        setNewNote("");
+      });
+  };
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => console.log(response.data))
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
